89b5e7ecf94875af3a8466a7a0a029b9435ed34b
seremshadrack <skemei660@gmail.com>
2022-11-27 22:37:56 +0300

    grade generator

---
+++ grade.js
@@ -0,0 +1,18 @@
+let gradeGen=prompt("input a number")
+let inptNum=parseInt("score")
+
+if (gradeGen>=80){
+return score ("A");
+}
+ else if (gradeGen>=70, 79){
+return score ("B");
+}
+else if(gradeGen>=60,69){
+    return score("C");
+}
+else if(gradeGen>=50,59){
+return score ("D")
+}
+else{
+    score ("E")
+}
